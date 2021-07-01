2a0e10f9589c6626f078e04653fbb3e0fcb01eca
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2021-07-01 17:54:31 +0700

    DEPC: new config telescope updated

--- nvim/.config/nvim/lua/modules/completion/config.lua
+++ nvim/.config/nvim/lua/modules/completion/config.lua
@@ -125,10 +125,17 @@ function config.telescope()
     require("telescope").setup {
         defaults = {
             prompt_prefix = "🔭 ",
-            prompt_position = "top",
+            layout_config = {
+                prompt_position = "top",
+                horizontal = {
+                    mirror = false
+                },
+                vertical = {
+                    mirror = false
+                }
+            },
             selection_caret = " ",
             sorting_strategy = "ascending",
-            results_width = 0.6,
             file_previewer = require "telescope.previewers".vim_buffer_cat.new,
             grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
             qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
