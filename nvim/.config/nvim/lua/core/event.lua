45e93166092f48446e2f9314ba286f7c249d36cf
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2021-07-08 16:13:21 +0700

    Remove event dotoo from ft org

--- nvim/.config/nvim/lua/core/event.lua
+++ nvim/.config/nvim/lua/core/event.lua
@@ -73,7 +73,6 @@ function autocmd.load_autocmds()
         ft = {
             {"FileType", "dashboard", "set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2"},
             {"BufNewFile,BufRead", "*.toml", " setf toml"},
-            {"BufNewFile,BufRead", "*.org", " setf dotoo"},
             {"TermOpen", "*", " setf bufhidden=hide | :startinsert"},
             {"TermOpen", "*", " setf nonumber norelativenumber"},
             {"FileType", "qf", "wincmd J"}
