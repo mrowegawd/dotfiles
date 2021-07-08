a3d8dbb59de119b9aad22520d3eef3765761d0c2
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2021-07-08 16:01:11 +0700

    Delete unused funcs

--- nvim/.config/nvim/lua/internal/util.lua
+++ nvim/.config/nvim/lua/internal/util.lua
@@ -161,17 +161,4 @@ Util.handleURL = function(isVisual)
     end
 end
 
--- local isDiffOpen = 1
-
--- Util.diffview = function()
---     -- need plugin sindrets/diffview.nvim
---     if isDiffOpen == 1 then
---         api.nvim_command([[silent!exec 'DiffviewOpen']])
---         isDiffOpen = 0
---     else
---         api.nvim_command([[silent!exec 'DiffviewClose']])
---         isDiffOpen = 1
---     end
--- end
-
 return Util
