9cc6572b00aa38541715e54f749fd292293babb1
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2023-10-11 04:37:10 +0700

    Navigation.lua configs updated..but WIP

--- nvim/.config/nvim/lua/r/plugins/navigation.lua
+++ nvim/.config/nvim/lua/r/plugins/navigation.lua
@@ -1,69 +1,21 @@
-local cmd, highlight, fn, icons = vim.cmd, as.highlight, vim.fn, as.ui.icons
+local cmd, highlight, icons = vim.cmd, as.highlight, as.ui.icons
 
 return {
-  -- OIL.NVIM (disabled)
-  {
-    "stevearc/oil.nvim", -- File manager
-    opts = {
-      keymaps = {
-        -- ["<C-c>"] = false,
-        -- ["<C-s>"] = "actions.save",
-        ["q"] = "actions.close",
-        ["H"] = "actions.toggle_hidden",
-        ["g?"] = "actions.show_help",
-        ["<CR>"] = "actions.select",
-        ["<C-v>"] = "actions.select_vsplit",
-        ["<C-s>"] = "actions.select_split",
-        ["<C-t>"] = "actions.select_tab",
-        ["<C-p>"] = "actions.preview",
-        ["<C-c>"] = "actions.close",
-        ["<C-l>"] = "actions.refresh",
-        ["<bs>"] = "actions.parent",
-        ["_"] = "actions.open_cwd",
-        ["`"] = "actions.cd",
-        ["~"] = "actions.tcd",
-        ["g."] = "actions.toggle_hidden",
-      },
-      buf_options = {
-        buflisted = false,
-      },
-      float = {
-        border = "none",
-      },
-      skip_confirm_for_simple_edits = true,
-    },
-    keys = {
-      {
-        "<localleader>e",
-        function()
-          require("oil").toggle_float "."
-        end,
-        desc = "Misc(oil): open File Explorer",
-      },
-      {
-        "<localleader>E",
-        function()
-          require("oil").toggle_float()
-        end,
-        desc = "Misc(oil): open File Explorer to current file",
-      },
-    },
-  },
   -- NEO-TREE
   {
     "nvim-neo-tree/neo-tree.nvim",
-    event = "VimEnter",
+    cmd = "Neotree",
     init = function()
       require("r.utils").disable_ctrl_i_and_o("NoNeoTree", { "neo-tree" })
-      vim.api.nvim_create_autocmd("BufEnter", {
-        desc = "Load NeoTree if entering a directory",
-        callback = function(args)
-          if fn.isdirectory(vim.api.nvim_buf_get_name(args.buf)) > 0 then
-            require("lazy").load { plugins = { "neo-tree.nvim" } }
-            vim.api.nvim_del_autocmd(args.id)
-          end
-        end,
-      })
+      --   vim.api.nvim_create_autocmd("BufEnter", {
+      --     desc = "Load NeoTree if entering a directory",
+      --     callback = function(args)
+      --       if fn.isdirectory(vim.api.nvim_buf_get_name(args.buf)) > 0 then
+      --         require("lazy").load { plugins = { "neo-tree.nvim" } }
+      --         vim.api.nvim_del_autocmd(args.id)
+      --       end
+      --     end,
+      --   })
     end,
     keys = {
       {
@@ -88,59 +40,25 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons",
-      {
-        "ten3roberts/window-picker.nvim",
-        name = "window-picker",
-        config = function()
-          local picker = require "window-picker"
-          picker.setup()
-          picker.pick_window = function()
-            return picker.select({ hl = "WindowPicker", prompt = "Pick window: " }, function(winid)
-              return winid or nil
-            end)
-          end
-        end,
+      -- {
+      --   "ten3roberts/window-picker.nvim",
+      --   name = "window-picker",
+      --   config = function()
+      --     local picker = require "window-picker"
+      --     picker.setup()
+      --     picker.pick_window = function()
+      --       return picker.select({ hl = "WindowPicker", prompt = "Pick window: " }, function(winid)
+      --         return winid or nil
+      --       end)
+      --     end
+      --   end,
+      -- },
       },
-    },
-    config = function()
-      highlight.plugin("NeoTree", {
-        theme = {
-          ["*"] = {
-            { NeoTreeNormal = { link = "PanelBackground" } },
-            { NeoTreeNormalNC = { link = "PanelBackground" } },
-            { NeoTreeCursorLine = { link = "CursorLine" } },
-            { NeoTreeRootName = { underline = false } },
-            { NeoTreeStatusLine = { link = "PanelSt" } },
-            { NeoTreeTabActive = { bg = { from = "PanelBackground" }, bold = true } },
-            {
-              NeoTreeTabInactive = { bg = { from = "PanelDarkBackground", alter = 0.15 }, fg = { from = "Comment" } },
-            },
-            { NeoTreeTabSeparatorActive = { inherit = "PanelBackground", fg = { from = "Comment" } } },
-            {
-              NeoTreeTabSeparatorInactive = {
-                inherit = "NeoTreeTabInactive",
-                fg = { from = "PanelDarkBackground", attr = "bg" },
-              },
-            },
-          },
-          -- NOTE: panel background colours don't get ignored by tint.nvim so avoid using them for now
-          horizon = {
-            { NeoTreeWinSeparator = { link = "WinSeparator" } },
-            { NeoTreeTabActive = { link = "VisibleTab" } },
-            { NeoTreeTabSeparatorActive = { link = "VisibleTab" } },
-            { NeoTreeTabInactive = { inherit = "Comment", italic = false } },
-            { NeoTreeTabSeparatorInactive = { bg = "bg", fg = "bg" } },
-          },
-        },
-      })
-
-      vim.g.neo_tree_remove_legacy_commands = 1
-
+    opts = function()
       local symbols = require("lspkind").symbol_map
       local lsp_kinds = as.ui.lsp.highlights
-
-      require("neo-tree").setup {
-        sources = { "filesystem", "git_status", "document_symbols" },
+      return {
+        sources = { "filesystem", "buffers", "git_status", "document_symbols" },
         source_selector = {
           -- winbar = true,
           separator_active = "",
@@ -243,7 +161,6 @@ return {
           end,
         },
         window = {
-
           mappings = {
             ["<2-LeftMouse>"] = "open",
             ["l"] = "child_or_open",
@@ -257,7 +174,7 @@ return {
             ["<c-s>"] = "split_with_window_picker",
             ["<c-v>"] = "vsplit_with_window_picker",
             ["<esc>"] = "revert_preview",
-            ["<c-c>"] = "clear_filter",
+            -- ["<c-c>"] = "clear_filter",
 
             ["zM"] = "close_all_nodes",
             ["zO"] = "expand_all_nodes",
@@ -267,95 +184,216 @@ return {
         },
       }
     end,
-  },
-  -- NVIM-TREE (disabled)
+    config = function(_, opts)
+      highlight.plugin("NeoTree", {
+        theme = {
+          ["*"] = {
+            { NeoTreeNormal = { link = "PanelBackground" } },
+            { NeoTreeNormalNC = { link = "PanelBackground" } },
+            { NeoTreeCursorLine = { link = "CursorLine" } },
+            { NeoTreeRootName = { underline = false } },
+            { NeoTreeStatusLine = { link = "PanelSt" } },
+            { NeoTreeTabActive = { bg = { from = "PanelBackground" }, bold = true } },
   {
-    "nvim-tree/nvim-tree.lua",
-    enabled = false,
-    cmd = {
-      "NvimTreeToggle",
-      "NvimTreeClose",
-      "NvimTreeFindFileToggle",
+              NeoTreeTabInactive = { bg = { from = "PanelDarkBackground", alter = 0.15 }, fg = { from = "Comment" } },
     },
-    config = function()
-      -- disable netrw at the very start of your init.lua (strongly advised)
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-
-      -- set termguicolors to enable highlight groups
-      vim.opt.termguicolors = true
-
-      local nvimtree = require "nvim-tree"
-
-      local options = {
-        actions = {
-          open_file = {
-            quit_on_open = true,
-          },
-        },
-        sort_by = "case_sensitive",
-        select_prompts = true,
-        view = {
-          adaptive_size = true,
-          mappings = {
-            list = {
-              { key = "u", action = "dir_up" },
+            { NeoTreeTabSeparatorActive = { inherit = "PanelBackground", fg = { from = "Comment" } } },
               {
-                key = { "<CR>", "<2-LeftMouse>", "l" },
-                action = "edit",
-                mode = "n",
+              NeoTreeTabSeparatorInactive = {
+                inherit = "NeoTreeTabInactive",
+                fg = { from = "PanelDarkBackground", attr = "bg" },
               },
-              {
-                key = { "<2-RightMouse>", "<C-}>" },
-                action = "cd",
-              },
-              { key = "<C-v>", action = "vsplit" },
-              { key = "<C-s>", action = "split" },
-              { key = "<C-t>", action = "tabnew" },
-              -- { key = "<", action = "prev_sibling" },
-              -- { key = ">", action = "next_sibling" },
-              { key = "P", action = "parent_node" },
-              { key = "<BS>", action = "close_node" },
-              { key = "<S-CR>", action = "close_node" },
-              { key = "<S-l>", action = "close_node" },
-              { key = "<Tab>", action = "preview" },
-              -- { key = "K", action = "first_sibling" },
-              -- { key = "J", action = "last_sibling" },
-              { key = "I", action = "toggle_ignored" },
-              { key = "H", action = "toggle_dotfiles" },
-              { key = "R", action = "refresh" },
-              { key = "a", action = "create" },
-              { key = "d", action = "remove" },
-              { key = "r", action = "rename" },
-              { key = "<C-r>", action = "full_rename" },
-              { key = "x", action = "cut" },
-              { key = "c", action = "copy" },
-              { key = "p", action = "paste" },
-              { key = "y", action = "copy_name" },
-              { key = "Y", action = "copy_path" },
-              { key = "gy", action = "copy_absolute_path" },
-              { key = "<", action = "dir_up" },
-              { key = "q", action = "close" },
-              { key = "g?", action = "toggle_help" },
-              { key = "gp", action = "prev_git_item" },
-              { key = "gn", action = "next_git_item" },
-              -- ["<CR>"] = ":YourVimFunction()<cr>",
-              -- ["u"] = ":lua require'some_module'.some_function()<cr>",
             },
           },
+          -- NOTE: panel background colours don't get ignored by tint.nvim so avoid using them for now
+          horizon = {
+            { NeoTreeWinSeparator = { link = "WinSeparator" } },
+            { NeoTreeTabActive = { link = "VisibleTab" } },
+            { NeoTreeTabSeparatorActive = { link = "VisibleTab" } },
+            { NeoTreeTabInactive = { inherit = "Comment", italic = false } },
+            { NeoTreeTabSeparatorInactive = { bg = "bg", fg = "bg" } },
+          },
         },
-        renderer = {
-          group_empty = true,
-        },
-        filters = {
-          dotfiles = true,
-        },
-        disable_netrw = false,
-        hijack_netrw = true,
-        respect_buf_cwd = true,
-      }
+      })
 
-      nvimtree.setup(options)
+      vim.g.neo_tree_remove_legacy_commands = 1
+
+      require("neo-tree").setup(opts)
+    end,
+  },
+  -- EDGY
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>ue",
+        function()
+          require("edgy").toggle()
+        end,
+        desc = "Misc(edgy): toggle",
+      },
+      -- stylua: ignore
+      { "<leader>uE", function() require("edgy").select() end, desc = "Misc(edgy): select window" },
+    },
+    opts = function()
+      local opts = {
+        bottom = {
+          {
+            ft = "toggleterm",
+            size = { height = 0.4 },
+            ---@diagnostic disable-next-line: unused-local
+            filter = function(buf, win)
+              return vim.api.nvim_win_get_config(win).relative == ""
+            end,
+              },
+              {
+            ft = "noice",
+            size = { height = 0.4 },
+            ---@diagnostic disable-next-line: unused-local
+            filter = function(buf, win)
+              return vim.api.nvim_win_get_config(win).relative == ""
+            end,
+              },
+          {
+            ft = "lazyterm",
+            title = "LazyTerm",
+            size = { height = 0.4 },
+            filter = function(buf)
+              return not vim.b[buf].lazyterm_cmd
+            end,
+            },
+          -- "Trouble",
+          { ft = "qf", title = "QuickFix" },
+          {
+            ft = "help",
+            size = { height = 20 },
+            -- don't open help files in edgy that we're editing
+            filter = function(buf)
+              return vim.bo[buf].buftype == "help"
+            end,
+          },
+          { ft = "spectre_panel", size = { height = 0.4 } },
+          { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
+        },
+        left = {
+          {
+            title = "Neo-Tree",
+            ft = "neo-tree",
+            filter = function(buf)
+              return vim.b[buf].neo_tree_source == "filesystem"
+            end,
+            pinned = true,
+            open = function()
+              vim.api.nvim_input "<esc><space>e"
+            end,
+            size = { height = 0.5 },
+        },
+          { title = "Neotest Summary", ft = "neotest-summary" },
+          {
+            title = "Neo-Tree Git",
+            ft = "neo-tree",
+            filter = function(buf)
+              return vim.b[buf].neo_tree_source == "git_status"
+            end,
+            pinned = true,
+            open = "Neotree position=right git_status",
+          },
+          {
+            title = "Neo-Tree Buffers",
+            ft = "neo-tree",
+            filter = function(buf)
+              return vim.b[buf].neo_tree_source == "buffers"
+            end,
+            pinned = true,
+            open = "Neotree position=top buffers",
+          },
+          "neo-tree",
+        },
+        keys = {
+          -- increase width
+          ["<c-Right>"] = function(win)
+            win:resize("width", 2)
+          end,
+          -- decrease width
+          ["<c-Left>"] = function(win)
+            win:resize("width", -2)
+          end,
+          -- increase height
+          ["<c-Up>"] = function(win)
+            win:resize("height", 2)
+          end,
+          -- decrease height
+          ["<c-Down>"] = function(win)
+            win:resize("height", -2)
+          end,
+        },
+      }
+      -- if as.has "symbols-outline.nvim" then
+      --   table.insert(opts.left, {
+      --     title = "Outline",
+      --     ft = "Outline",
+      --     pinned = true,
+      --     open = "SymbolsOutline",
+      --   })
+      -- end
+      return opts
+    end,
+  },
+  -- use edgy's selection window
+  {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        get_selection_window = function()
+          require("edgy").goto_main()
+          return 0
+        end,
+      },
+    },
+  },
+
+  -- prevent neo-tree from opening files in edgy windows
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.open_files_do_not_replace_types = opts.open_files_do_not_replace_types
+        or { "terminal", "Trouble", "qf", "Outline" }
+      table.insert(opts.open_files_do_not_replace_types, "edgy")
+    end,
+  },
+
+  -- Fix bufferline offsets when edgy is loaded
+  {
+    "akinsho/bufferline.nvim",
+    optional = true,
+    opts = function()
+      local Offset = require "bufferline.offset"
+      if not Offset.edgy then
+        local get = Offset.get
+        Offset.get = function()
+          if package.loaded.edgy then
+            local layout = require("edgy.config").layout
+            local ret = { left = "", left_size = 0, right = "", right_size = 0 }
+            for _, pos in ipairs { "left", "right" } do
+              local sb = layout[pos]
+              if sb and #sb.wins > 0 then
+                local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
+                ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
+                ret[pos .. "_size"] = sb.bounds.width
+              end
+            end
+            ret.total_size = ret.left_size + ret.right_size
+            if ret.total_size > 0 then
+              return ret
+            end
+          end
+          return get()
+        end
+        Offset.edgy = true
+      end
     end,
   },
 }
