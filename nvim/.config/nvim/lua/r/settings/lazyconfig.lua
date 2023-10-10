bd9f7c8a2033017b23ba603587196d811468e8c4
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2023-10-11 04:31:55 +0700

    update(lazy): config

--- nvim/.config/nvim/lua/r/settings/lazyconfig.lua
+++ nvim/.config/nvim/lua/r/settings/lazyconfig.lua
@@ -17,6 +17,9 @@ end
 opt.rtp:prepend(lazypath)
 
 require("lazy").setup {
+  -- debug = true,
+  lockfile = data .. "/lazy-lock.json",
+  concurrency = 20,
   spec = {
     { import = "r.plugins" },
     { import = "r.plugins.extras.db" },
@@ -41,10 +44,29 @@ require("lazy").setup {
     cmd = "terminal_git",
   },
   ui = {
-    border = "single",
+    wrap = true, -- wrap the lines in the ui
+    border = "rounded",
+  },
+  git = {
+    log = { "--since=3 days ago" }, -- show commits from the last 3 days
+    timeout = 120, -- kill processes that take more than 2 minutes
+    url_format = "https://github.com/%s.git",
   },
   performance = {
+    cache = {
+      enabled = true,
+      path = vim.fn.stdpath "cache" .. "/lazy/cache",
+      -- Once one of the following events triggers, caching will be disabled.
+      -- To cache all modules, set this to `{}`, but that is not recommended.
+      -- The default is to disable on:
+      --  * VimEnter: not useful to cache anything else beyond startup
+      --  * BufReadPre: this will be triggered early when opening a file from the command line directly
+      disable_events = { "VimEnter", "BufReadPre" },
+      ttl = 3600 * 24 * 5, -- keep unused modules for up to 5 days
+    },
+    reset_packpath = true, -- reset the package path to improve startup time
     rtp = {
+      reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
       disabled_plugins = {
         "gzip",
         "matchit",
@@ -59,6 +81,4 @@ require("lazy").setup {
       },
     },
   },
-  lockfile = data .. "/lazy-lock.json",
-  -- debug = true,
 }
