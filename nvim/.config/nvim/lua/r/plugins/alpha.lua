708ccfc2c5482a14d6db357989fd0c91492ece37
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2023-10-13 04:49:49 +0700

    Add dashboard and disable alpha

--- nvim/.config/nvim/lua/r/plugins/alpha.lua
+++ nvim/.config/nvim/lua/r/plugins/alpha.lua
@@ -1,14 +1,7 @@
-return {
-  -- ALPHA
+-- Get ascii generator:
+-- https://lachlanarthur.github.io/Braille-ASCII-Art/
+local ascii_img = {
   {
-    "goolord/alpha-nvim",
-    event = "VimEnter",
-    config = function()
-      local alpha = require "alpha"
-
-      -- Get ascii generator:
-      -- https://lachlanarthur.github.io/Braille-ASCII-Art/
-      local one_punch_man = {
         [[]],
         [[                 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡤⠤⠤⠤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠖⠈⠉⠉⠁⠒⠤⡀⠀⠀ ]],
         [[                 ⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠾⠿⠶⠀⠀⠀⠀⠀⠈⠑⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⠁⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀ ]],
@@ -33,10 +26,8 @@ return {
         [[                 ⣯⣽⣿⣿⢚⣻⣿⣷⣿⣫⣟⣻⣿⣿⣿⣷⡇⢸⣿⡄⢸⣿⣿⣿⣿⣿⣿⡿⣟⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ]],
         [[                 ⣾⣶⣿⣾⣯⣥⣿⣿⣟⣿⣯⣿⣿⣿⣿⣿⣿⣇⣸⣿⣃⣸⣿⣿⣿⣿⣿⣷⣿⣿⣿⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
         [[]],
-      }
-
-      ---@diagnostic disable-next-line: unused-local
-      local batman = {
+  },
+  {
         [[]],
         [[                     Tb.          Tb.                                ]],
         [[                     :$$b.        $$$b.                              ]],
@@ -71,11 +62,75 @@ return {
         [[ d$$$$$$$$$$pp..__..gg$$$$$$$$$$$$$$$$$$$$$$$$$$$                    ]],
         [[d$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$p._            .gp. ]],
         [[$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$p._.ggp._.d$$$$b]],
+  },
+}
+
+return {
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function()
+      -- logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+      local opts = {
+        theme = "doom",
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+          -- number = false,
+          -- relativenumber = false,
+        },
+        config = {
+          -- header = vim.split(ascii_img[1], "\n"),
+          header = ascii_img[1],
+          center = {
+            { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
+            { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
+            { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
+            { action = "FzfLua live_grep", desc = " Find text", icon = " ", key = "g" },
+            { action = "e $MYVIMRC", desc = " Config", icon = " ", key = "c" },
+            { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "l" },
+            { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "L" },
+            { action = "qa", desc = " Quit", icon = " ", key = "q" },
+          },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
       }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+      end
+
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "DashboardLoaded",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      return opts
+    end,
+  },
+  -- ALPHA
+  {
+    "goolord/alpha-nvim",
+    enabled = false,
+    event = "VimEnter",
+    config = function()
+      local alpha = require "alpha"
 
       local header = {
         type = "text",
-        val = one_punch_man,
+        val = ascii_img[1],
         opts = {
           position = "center",
           hl = "AlphaHeader",
