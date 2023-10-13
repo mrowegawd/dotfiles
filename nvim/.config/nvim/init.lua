b444f7c581e2095333115e0b2583a1c5bb5e09de
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2023-10-13 13:17:16 +0700

    Update(init.lua): remove unwanted lines

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,13 @@
--- Enable experimental lua module loader
-vim.loader.enable()
-
 require "r.settings.globals"
 require "r.settings.options"
 require "r.settings.ui"
 require "r.settings.highlights"
 require "r.settings.lazyconfig"
 
--- Load colorscheme
+-- cfilter plugin allows filtering down an existing quickfix list
+vim.cmd.packadd "cfilter"
 
+vim.keymap.set("n", "<leader>rL", "<Cmd>Lazy<CR>", { desc = "Misc(lazy): manage" })
+
+-- Load colorscheme
 as.pcall("theme failed to load because", vim.cmd.colorscheme, as.colorscheme)
