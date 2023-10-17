551da053a9557803bf6fb44863b2bac1ebacbee4
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2023-10-18 00:24:10 +0700

    update(tmux.conf): mappings

--- nvim/.config/nvim/lua/r/plugins/extras/formatter/prettier.lua
+++ nvim/.config/nvim/lua/r/plugins/extras/formatter/prettier.lua
@@ -1,119 +1,41 @@
-local Util = require "r.utils"
-
-local M = {}
-
----@type ConformOpts
-local conform_opts = {}
-
----@param opts ConformOpts
-function M.setup(_, opts)
-  for name, formatter in pairs(opts.formatters or {}) do
-    if type(formatter) == "table" then
-      ---@diagnostic disable-next-line: undefined-field
-      if formatter.extra_args then
-        ---@diagnostic disable-next-line: undefined-field
-        formatter.prepend_args = formatter.extra_args
-        Util.deprecate(("opts.formatters.%s.extra_args"):format(name), ("opts.formatters.%s.prepend_args"):format(name))
-      end
-    end
-  end
-
-  for _, key in ipairs { "format_on_save", "format_after_save" } do
-    if opts[key] then
-      Util.warn(
-        ("Don't set `opts.%s` for `conform.nvim`.\n**LazyVim** will use the conform formatter automatically"):format(
-          key
-        )
-      )
-      ---@diagnostic disable-next-line: no-unknown
-      opts[key] = nil
-    end
-  end
-  conform_opts = opts
-  require("conform").setup(opts)
-end
-
 return {
   {
-    "stevearc/conform.nvim",
-    dependencies = { "mason.nvim" },
-    lazy = true,
-    cmd = "ConformInfo",
-    keys = {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "prettier")
+    end,
+  },
       {
-        "<leader>cF",
-        function()
-          require("conform").format { formatters = { "injected" } }
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require "null-ls"
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.prettier)
         end,
-        mode = { "n", "v" },
-        desc = "Format Injected Langs",
-      },
-    },
-    init = function()
-      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-      -- Install the conform formatter on VeryLazy
-      require("r.utils").on_very_lazy(function()
-        require("r.utils").format.register {
-          name = "conform.nvim",
-          priority = 100,
-          primary = true,
-          format = function(buf)
-            require("conform").format(Util.merge({
-              timeout_ms = conform_opts.format.timeout_ms,
-              async = conform_opts.format.async,
-              quiet = conform_opts.format.quiet,
-            }, { bufnr = buf }))
-          end,
-          sources = function(buf)
-            local ret = require("conform").list_formatters(buf)
-            return vim.tbl_map(function(v)
-              return v.name
-            end, ret)
-          end,
-        }
-      end)
-    end,
-    opts = function()
-      local plugin = require("lazy.core.config").plugins["conform.nvim"]
-      if plugin.config ~= M.setup then
-        Util.error({
-          "Don't set `plugin.config` for `conform.nvim`.\n",
-          "This will break **LazyVim** formatting.\n",
-          "Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
-        }, { title = "LazyVim" })
-      end
-      ---@class ConformOpts
-      local opts = {
-        -- LazyVim will use these options when formatting with the conform.nvim formatter
-        format = {
-          timeout_ms = 3000,
-          async = false, -- not recommended to change
-          quiet = false, -- not recommended to change
         },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
         formatters_by_ft = {
-          lua = { "stylua" },
-          fish = { "fish_indent" },
-          sh = { "shfmt" },
+        ["javascript"] = { "prettier" },
+        ["javascriptreact"] = { "prettier" },
+        ["typescript"] = { "prettier" },
+        ["typescriptreact"] = { "prettier" },
+        ["vue"] = { "prettier" },
+        ["css"] = { "prettier" },
+        ["scss"] = { "prettier" },
+        ["less"] = { "prettier" },
+        ["html"] = { "prettier" },
+        ["json"] = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        ["yaml"] = { "prettier" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        ["graphql"] = { "prettier" },
+        ["handlebars"] = { "prettier" },
         },
-        -- LazyVim will merge the options you set here with builtin formatters.
-        -- You can also define any custom formatters here.
-        formatters = {
-          injected = { options = { ignore_errors = true } },
-          -- # Example of using dprint only when a dprint.json file is present
-          -- dprint = {
-          --   condition = function(ctx)
-          --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-          --   end,
-          -- },
-          --
-          -- # Example of using shfmt with extra args
-          -- shfmt = {
-          --   extra_args = { "-i", "2", "-ci" },
-          -- },
         },
-      }
-      return opts
-    end,
-    config = M.setup,
   },
 }
