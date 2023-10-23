09674729fc2e7285d339793a61f6fcb68d387a60
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2023-10-24 01:37:22 +0700

    feat(uisec): remove 'as.'

--- nvim/.config/nvim/lua/r/utils/uisec.lua
+++ nvim/.config/nvim/lua/r/utils/uisec.lua
@@ -134,7 +134,7 @@ function M.decorations_set_colorcolumn(bufnr, fn)
       return
     end
     local ccol = decor.ft or decor.bt or ""
-    local virtcolumn = not as.falsy(ccol) and ccol or "+1"
+    local virtcolumn = not Util.cmd.falsy(ccol) and ccol or "+1"
     if vim.is_callable(fn) then
       fn(virtcolumn)
     end
