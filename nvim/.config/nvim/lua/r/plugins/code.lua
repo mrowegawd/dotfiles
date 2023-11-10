f712256a51e2f238be78ec3a15bf313beb80ea5d
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2023-11-10 11:39:33 +0700

    feat(code): refix cmp-git

--- nvim/.config/nvim/lua/r/plugins/code.lua
+++ nvim/.config/nvim/lua/r/plugins/code.lua
@@ -60,7 +60,6 @@ return {
       vim.g.matchup_matchparen_offscreen = {} -- disable status bar icon
     end,
   },
-  { "rcarriga/cmp-dap" },
   -- CMP
   {
     "hrsh7th/nvim-cmp",
@@ -69,10 +68,11 @@ return {
       "davidsierradz/cmp-conventionalcommits",
       "dmitmel/cmp-cmdline-history",
       "hrsh7th/cmp-buffer",
+      "rcarriga/cmp-dap",
       "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
-      "petertriho/cmp-git",
+      { "petertriho/cmp-git", opts = { filetypes = { "gitcommit", "NeogitCommitMessage" } } },
       "lukas-reineke/cmp-under-comparator",
       "saadparwaiz1/cmp_luasnip",
       { "abecodes/tabout.nvim", opts = { ignore_beginning = false, completion = false } },
@@ -192,7 +192,6 @@ return {
               end,
             },
           },
-          { name = "git" },
         },
       }
     end,
@@ -379,39 +378,6 @@ return {
       }
     end,
   },
-  -- MINI-SURROUND
-  {
-    "echasnovski/mini.surround",
-    keys = function(_, keys)
-      -- Populate the keys based on the user's options
-      local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-      local mappings = {
-        { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-        { opts.mappings.delete, desc = "Delete surrounding" },
-        { opts.mappings.find, desc = "Find right surrounding" },
-        { opts.mappings.find_left, desc = "Find left surrounding" },
-        { opts.mappings.highlight, desc = "Highlight surrounding" },
-        { opts.mappings.replace, desc = "Replace surrounding" },
-        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-      }
-      mappings = vim.tbl_filter(function(m)
-        return m[1] and #m[1] > 0
-      end, mappings)
-      return vim.list_extend(mappings, keys)
-    end,
-    opts = {
-      mappings = {
-        add = "gza", -- Add surrounding in Normal and Visual modes
-        delete = "gzd", -- Delete surrounding
-        find = "gzf", -- Find surrounding (to the right)
-        find_left = "gzF", -- Find surrounding (to the left)
-        highlight = "gzh", -- Highlight surrounding
-        replace = "gzc", -- Replace surrounding
-        update_n_lines = "gzn", -- Update `n_lines`
-      },
-    },
-  },
   -- COPILOT (disabled)
   {
     "zbirenbaum/copilot.lua",
@@ -548,7 +514,6 @@ return {
     "LintaoAmons/scratch.nvim",
     cmd = { "Scratch", "ScratchOpen" },
     keys = {
-
       {
         "<Localleader>oS",
         "<CMD>Scratch<CR>",
