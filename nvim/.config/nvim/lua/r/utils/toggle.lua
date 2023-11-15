bc7a81d5fd892d941e2aa463d2e85bad839bcb5b
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2023-11-15 13:27:23 +0700

    remove qf stuff

--- nvim/.config/nvim/lua/r/utils/toggle.lua
+++ nvim/.config/nvim/lua/r/utils/toggle.lua
@@ -1,57 +1,6 @@
-local api, cmd = vim.api, vim.cmd
-
 local Util = require "r.utils"
 
 local M = {}
-
-local function is_vim_list_open()
-  for _, win in ipairs(api.nvim_list_wins()) do
-    local buf = api.nvim_win_get_buf(win)
-    local location_list = vim.fn.getloclist(0, { filewinid = 0 })
-    local is_loc_list = location_list.filewinid > 0
-    if vim.bo[buf].filetype == "qf" or is_loc_list then
-      return true
-    end
-  end
-  return false
-end
-
--- Usage: toggle_list "quickfix" or "location"
-local function toggle_list(list_type, kill)
-  if kill then
-    return cmd [[q]]
-  end
-
-  local is_location_target = list_type == "location"
-  local cmd_ = is_location_target and { "lclose", "lopen" } or { "cclose", "copen" }
-  local is_open = is_vim_list_open()
-  if is_open then
-    return vim.cmd[cmd_[1]]()
-  end
-  local list = is_location_target and vim.fn.getloclist(0) or vim.fn.getqflist()
-  if vim.tbl_isempty(list) then
-    local msg_prefix = (is_location_target and "Location" or "QuickFix")
-    return Util.info(msg_prefix .. " List is Empty.", vim.log.levels.WARN)
-  end
-
-  local winnr = vim.fn.winnr()
-  cmd[cmd_[2]]()
-  if vim.fn.winnr() ~= winnr then
-    cmd.wincmd "p"
-  end
-end
-
-function M.toggle_qf()
-  toggle_list "quickfix"
-end
-
-function M.toggle_loc()
-  toggle_list "location"
-end
-
-function M.kill_loc_qf()
-  toggle_list("none", true)
-end
 
 local function bool2str(bool)
   return bool and "on" or "off"
