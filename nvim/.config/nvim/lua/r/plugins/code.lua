498808cf72b08ead566e0e39db2ed298ba25f8a8
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2023-11-28 05:15:54 +0700

    fix(cmp): `<cr>` -> `select=false`

--- nvim/.config/nvim/lua/r/plugins/code.lua
+++ nvim/.config/nvim/lua/r/plugins/code.lua
@@ -201,8 +201,7 @@ return {
           ["<S-TAB>"] = cmp.mapping(shift_tab, { "i", "s" }),
           ["<c-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "c", "i" }),
           ["<c-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "c", "i" }),
-          -- TODO: ini seharusnya kalau tidak di select jgn di confim
-          -- ["<cr>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
+          ["<cr>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false },
         },
 
         experimental = { ghost_text = false },
@@ -654,6 +653,7 @@ return {
 
       { "rC", "<Cmd> RmuxKillAllPanes <CR>" },
       { "rA", "<Cmd> RmuxRunTaskAll <CR>" },
+      { "rg", "<Cmd> RmuxGrepErr <CR>" },
 
       { "re", "<Cmd> RmuxEDITConfig <CR>" },
 
