4f29ae3e49cb2495fdc18ec420f17ab48f284b23
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2023-12-05 19:31:48 +0700

    fix(eslint): it works now

--- nvim/.config/nvim/lua/r/plugins/extras/formatter/prettier.lua
+++ nvim/.config/nvim/lua/r/plugins/extras/formatter/prettier.lua
@@ -17,44 +17,25 @@ return {
   {
     "stevearc/conform.nvim",
     optional = true,
-    opts = function(_, opts)
-      local function add_formatters(tbl)
-        for ft, formatters in pairs(tbl) do
-          if opts.formatters_by_ft[ft] == nil then
-            opts.formatters_by_ft[ft] = formatters
-          else
-            vim.list_extend(opts.formatters_by_ft[ft], formatters)
-          end
-        end
-      end
-      add_formatters {
+    opts = {
+      formatters_by_ft = {
         ["javascript"] = { "prettier" },
         ["javascriptreact"] = { "prettier" },
         ["typescript"] = { "prettier" },
-        ["markdown.mdx"] = { "prettier" },
-        ["graphql"] = { "prettier" },
-        ["handlebars"] = { "prettier" },
-        ["json"] = { "prettier" },
-        ["jsonc"] = { "prettier" },
-        ["html"] = { "prettier" },
+        ["typescriptreact"] = { "prettier" },
+        ["vue"] = { "prettier" },
         ["css"] = { "prettier" },
         ["scss"] = { "prettier" },
         ["less"] = { "prettier" },
-      }
-    end,
-    --   opts = {
-    --     formatters_by_ft = {
-    --       ["javascript"] = { "prettier" },
-    --       ["javascriptreact"] = { "prettier" },
-    --       ["typescript"] = { "prettier" },
-    --       ["typescriptreact"] = { "prettier" },
-    --       ["vue"] = { "prettier" },
-    --       ["yaml"] = { "prettier" },
-    --       ["markdown"] = { "prettier" },
-    --       ["markdown.mdx"] = { "prettier" },
-    --       ["graphql"] = { "prettier" },
-    --       ["handlebars"] = { "prettier" },
-    --     },
-    --   },
+        ["html"] = { "prettier" },
+        ["json"] = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        ["yaml"] = { "prettier" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        ["graphql"] = { "prettier" },
+        ["handlebars"] = { "prettier" },
+      },
+    },
   },
 }
