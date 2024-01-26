8440a93ba3ff694d4cca7e68d54ed519c119df8f
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-01-27 00:51:16 +0700

    neorg need netrwPlugin

--- nvim/.config/nvim/lua/r/config/lazyconfig.lua
+++ nvim/.config/nvim/lua/r/config/lazyconfig.lua
@@ -71,7 +71,7 @@ require("lazy").setup {
         "gzip",
         "matchit",
         "matchparen",
-        "netrwPlugin",
+        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
