c57ff7658fb259d1f412d91b4101d0c7e2db036c
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-03-26 11:20:33 +0700

    use new font 'monospace' from google

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -190,6 +190,8 @@ config.hyperlink_rules = Hyperlinks
 -- ├┤ FONTS ├───────────────────────────────────────────────────────────┤
 config.line_height = 0.9 -- dan juga ini
 -- font_shaper = "Harfbuzz",
+
+-- Download font: https://monaspace.githubnext.com/
 config.harfbuzz_features = { "calt=0" }
 config.adjust_window_size_when_changing_font_size = false
 config.font_rules = {
