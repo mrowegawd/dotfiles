f89edc3b6b2ae7f5f4b44e031bd500017b315d4a
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-04-22 03:01:39 +0700

    set mapping for launch debugging for lua

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -1,4 +1,4 @@
--- local keymap = vim.keymap
+local keymap = vim.keymap
 -- local buffer_dir = vim.fn.expand "%:p:h"
 
 vim.opt.tabstop = 2
@@ -29,7 +29,6 @@ RUtils.write_and_source(0)
 --   require("osv").run_this()
 -- end, { buffer = true, desc = "Debug: adapter lua [nlua]" })
 
--- keymap.set("n", "<Leader>daL", function()
---   require("osv").launch { port = 8086 }
--- end, { buffer = true, desc = "Debug: launch debug lua [nlua]" })
-
+keymap.set("n", "<Leader>dD", function()
+  require("osv").launch { port = 8086 }
+end, { buffer = true, desc = "Debug: launch debug lua [nlua]" })
