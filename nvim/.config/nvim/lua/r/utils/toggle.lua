397a8375ed8478af49763feb2e5c83c55a73029f
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-05-25 03:33:14 +0700

    ref(inlay_hint): more logic

--- nvim/.config/nvim/lua/r/utils/toggle.lua
+++ nvim/.config/nvim/lua/r/utils/toggle.lua
@@ -56,8 +56,20 @@ function M.semantic_tokens(bufnr)
   end
 end
 
-function M.inlay_hints(buf)
-  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { buf })
+---@param buf? number
+---@param value? boolean
+function M.inlay_hints(buf, value)
+  -- vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { buf })
+
+  local ih = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
+  if type(ih) == "function" then
+    ih(buf, value)
+  elseif type(ih) == "table" and ih.enable then
+    if value == nil then
+      value = not ih.is_enabled { bufnr = buf or 0 }
+    end
+    ih.enable(value, { bufnr = buf })
+  end
 end
 
 function M.codelens()
