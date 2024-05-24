e45341325b24f3062bb67f9d5574aeb601df3f9a
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-05-25 04:12:41 +0700

    add `codewindow`

--- nvim/.config/nvim/lua/r/plugins/ui.lua
+++ nvim/.config/nvim/lua/r/plugins/ui.lua
@@ -2,6 +2,42 @@ local fn = vim.fn
 local Highlight = require "r.settings.highlights"
 
 return {
+  -- CODEWINDOW
+  {
+    "gorbit99/codewindow.nvim",
+    lazy = false,
+    keys = {
+      {
+        "<leader>um",
+        function()
+          require("codewindow").toggle_minimap()
+        end,
+        desc = "Misc: toggle minimap [codewindow]",
+      },
+    },
+    opts = {
+      auto_enable = false,
+      minimap_width = 20,
+      width_multiplier = 2,
+      screen_bounds = "background",
+      window_border = "none",
+      relative = "win",
+      exclude_filetypes = {
+        "dashboard",
+        "neo-tree",
+        "lazy",
+        "lazyterm",
+        "mason",
+        "help",
+        "checkhealth",
+        "lspinfo",
+        "noice",
+        "Trouble",
+        "fish",
+        "zsh",
+      },
+    },
+  },
   -- INDENTMINI
   {
     "nvimdev/indentmini.nvim",
