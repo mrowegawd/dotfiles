d09f1e8f4dfcacdb536d723f9273c638c44df600
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-06-06 14:29:31 +0700

    fix(heirline): move object `opts` to field `opts`

--- nvim/.config/nvim/lua/r/plugins/statusline.lua
+++ nvim/.config/nvim/lua/r/plugins/statusline.lua
@@ -4,24 +4,28 @@ return {
     "rebelot/heirline.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "LazyFile",
-    config = function()
+    opts = function()
       local comp = require "r.plugins.colorthemes.heirline.components"
-      require("heirline").setup {
+      return {
         statusline = {
           comp.status_active_left,
           comp.status_not_active,
         },
-
-        -- opts = {
-        --   disable_winbar_cb = function(args)
-        --     return not require("astronvim.utils.buffer").is_valid(args.buf)
-        --       or status.condition.buffer_matches({
-        --         buftype = { "terminal", "prompt", "nofile", "help", "quickfix" },
-        --         filetype = { "NvimTree", "neo%-tree", "dashboard", "Outline", "aerial" },
-        --       }, args.buf)
-        --   end,
-        -- },
       }
+    end,
+    config = function(_, opts)
+      require("heirline").setup(opts)
+
+      -- opts = {
+      --   disable_winbar_cb = function(args)
+      --     return not require("astronvim.utils.buffer").is_valid(args.buf)
+      --       or status.condition.buffer_matches({
+      --         buftype = { "terminal", "prompt", "nofile", "help", "quickfix" },
+      --         filetype = { "NvimTree", "neo%-tree", "dashboard", "Outline", "aerial" },
+      --       }, args.buf)
+      --   end,
+      -- },
+      -- }
 
       local group = vim.api.nvim_create_augroup("Heirline", { clear = true })
       vim.api.nvim_create_autocmd("FileType", {
