1804a5061850ac83f58250c04bac29a9430926f1
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-06-20 01:17:57 +0700

    fix(ui): clean up

--- nvim/.config/nvim/lua/r/utils/ui.lua
+++ nvim/.config/nvim/lua/r/utils/ui.lua
@@ -156,14 +156,15 @@ function M.statuscolumn()
   local is_num = vim.wo[win].number
   local is_relnum = vim.wo[win].relativenumber
   if (is_num or is_relnum) and vim.v.virtnum == 0 then
-    -- if vim.v.relnum == 0 then
-    --   components[2] = is_num and "%l" or "%r" -- the current line
-    -- else
-    --   components[2] = is_relnum and "%r" or "%l" -- other lines
-    -- end
-    -- print "yes"
-    -- components[2] = is_relnum and "%r" or "%l" -- other lines
-    components[2] = is_num and "%l" or "%r" -- the current line
+    if vim.fn.has "nvim-0.11" == 1 then
+      components[2] = "%l"
+    else
+      if vim.v.relnum == 0 then
+        components[2] = is_num and "%l" or "%r" -- the current line
+      else
+        components[2] = is_relnum and "%r" or "%l" -- other lines
+      end
+    end
     components[2] = "%=" .. components[2] .. " " -- right align
   end
 
