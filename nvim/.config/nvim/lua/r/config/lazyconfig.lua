1565c7b5d3edf916988dd3bc46eb28aaa457ff4c
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-06-28 21:18:32 +0700

    fix(lazyconfig): clean up conf

--- nvim/.config/nvim/lua/r/config/lazyconfig.lua
+++ nvim/.config/nvim/lua/r/config/lazyconfig.lua
@@ -31,36 +31,13 @@ require("lazy").setup {
   },
   change_detection = { notify = false },
   install = {
-    colorscheme = { "terafox" },
     missing = true,
-  },
-  checker = {
-    enabled = true,
-    concurrency = 30,
-    notify = false,
-    frequency = 3600, -- check for updates every hour
   },
   ui = {
     wrap = true, -- wrap the lines in the ui
     border = "rounded",
   },
-  git = {
-    log = { "--since=3 days ago" }, -- show commits from the last 3 days
-    timeout = 120, -- kill processes that take more than 2 minutes
-    url_format = "https://github.com/%s.git",
-  },
   performance = {
-    cache = {
-      enabled = true,
-      path = vim.fn.stdpath "cache" .. "/lazy/cache",
-      -- Once one of the following events triggers, caching will be disabled.
-      -- To cache all modules, set this to `{}`, but that is not recommended.
-      -- The default is to disable on:
-      --  * VimEnter: not useful to cache anything else beyond startup
-      --  * BufReadPre: this will be triggered early when opening a file from the command line directly
-      -- disable_events = { "VimEnter", "BufReadPre" },
-      -- ttl = 3600 * 24 * 5, -- keep unused modules for up to 5 days
-    },
     reset_packpath = true, -- reset the package path to improve startup time
     rtp = {
       reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
