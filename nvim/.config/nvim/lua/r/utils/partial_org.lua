fb4ef7d7fa09cd992890d4742cb84c80d3651f06
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-07-11 16:10:36 +0700

    fix(partial_org): clean up

--- nvim/.config/nvim/lua/r/utils/partial_org.lua
+++ nvim/.config/nvim/lua/r/utils/partial_org.lua
@@ -5,17 +5,15 @@ require "r.utils"
 return {
   org_agenda_files = {
     fmt("%s/orgmode/gtd/*", RUtils.config.path.wiki_path),
+    fmt("%s/orgmode/gym/*", RUtils.config.path.wiki_path),
     fmt("%s/orgmode/habit/*", RUtils.config.path.wiki_path),
     fmt("%s/orgmode/day-to-remember/*", RUtils.config.path.wiki_path),
   },
   org_default_notes_file = fmt("%s/orgmode/gtd/refile.org", RUtils.config.path.wiki_path),
   notifications = {
-    -- reminder_time = { 0, 1, 5, 10 },
-    -- repeater_reminder_time = { 0, 1, 5, 10 },
-    -- deadline_warning_reminder_time = { 0, 5 },
-    reminder_time = { 0, 1, 5, 50 },
+    reminder_time = { 0, 1, 5 },
     deadline_warning_reminder_time = { 10, 5, 0, -5 },
-    repeater_reminder_time = { 30, 20, 10, 5, 0 },
+    repeater_reminder_time = { 10, 5, 0 },
     cron_notifier = function(tasks)
       for _, task in ipairs(tasks) do
         local title = fmt("%s (%s)", task.category, task.humanized_duration)
@@ -26,6 +24,8 @@ return {
         if vim.fn.executable "dunstify" == 1 then
           vim.uv.spawn("dunstify", {
             args = {
+              "-t",
+              "8000",
               fmt("[%s %s]\n%s:", task.type, date, title),
               fmt("<b><span foreground='#7ba05b'>%s</span></b>", subtitle),
               fmt("--icon=%s/.config/miscxrdb/icons/bell.png", os.getenv "HOME"),
