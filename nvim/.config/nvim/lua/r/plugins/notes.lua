4de8ca7e3e1e8dc9025933a2ab4be958b7e01559
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-07-25 06:04:29 +0700

    fix(obsidian): get rid `conceal` warning when opening obsidian

--- nvim/.config/nvim/lua/r/plugins/notes.lua
+++ nvim/.config/nvim/lua/r/plugins/notes.lua
@@ -506,6 +506,10 @@ return {
         min_chars = 2,
       },
 
+      ui = {
+        enable = false, -- set to false to disable all additional syntax features
+      },
+
       mappings = {
         ["<c-c>"] = {
           action = function()
@@ -530,85 +534,6 @@ return {
           require("r.keymaps.note").neorg_mappings_ft(api.nvim_get_current_buf())
         end,
       })
-    end,
-  },
-  -- HEADLINES.NVIM
-  {
-    "lukas-reineke/headlines.nvim",
-    ft = { "markdown", "norg", "rmd", "org" },
-    opts = function()
-      Highlight.plugin("headlines", {
-        theme = {
-          ["*"] = {
-            { Dash = { bg = "NONE", bold = true } },
-            { CodeBlock = { bg = { from = "Normal", attr = "bg", alter = 0.2 } } },
-            -- {
-            --   Headline1 = {
-            --     bg = "#332F46",
-            --     fg = { from = "PreProc", attr = "fg", alter = 1 },
-            --     bold = true,
-            --   },
-            -- },
-            -- {
-            --   Headline2 = {
-            --     bg = "#3F2F46",
-            --     fg = { from = "PreProc", attr = "fg", alter = 0.8 },
-            --     bold = true,
-            --   },
-            -- },
-            --
-            -- {
-            --   Headline3 = {
-            --     bg = "#462F42",
-            --     fg = { from = "PreProc", attr = "fg", alter = 0.6 },
-            --     bold = true,
-            --   },
-            -- },
-            -- {
-            --
-            --   Headline4 = {
-            --     bg = "#462F37",
-            --     fg = { from = "PreProc", attr = "fg", alter = 0.4 },
-            --     bold = true,
-            --   },
-            -- },
-            -- {
-            --   Headline5 = {
-            --     bg = "#46332F",
-            --     fg = { from = "PreProc", attr = "fg", alter = 0.2 },
-            --     bold = true,
-            --   },
-            -- },
-            -- {
-            --   Headline6 = {
-            --     -- bg = { from = "Normal", attr = "bg" },
-            --     bg = "#463F2F",
-            --     -- bg = { from = "Normal", attr = "bg", alter = -0.2 },
-            --     fg = { from = "PreProc", attr = "fg", alter = 0 },
-            --     bold = true,
-            --   },
-            -- },
-          },
-        },
-      })
-      return {
-        org = { headline_highlights = false },
-        norg = { headline_highlights = false, codeblock_highlight = false },
-        markdown = {
-          -- headline_highlights = { "Headline1", "Headline2", "Headline3", "Headline4", "Headline5" },
-          headline_highlights = false,
-          -- fat_headline_lower_string = "▔",
-          codeblock_highlight = "CodeBlock1",
-        },
-        fat_headline_lower_string = "▔",
-      }
-    end,
-    config = function(_, opts)
-      -- PERF: schedule to prevent headlines slowing down opening a file
-      vim.schedule(function()
-        require("headlines").setup(opts)
-        require("headlines").refresh()
-      end)
     end,
   },
   -- SNIPRUN
