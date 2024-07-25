a465d92c9ef7d30507ca9641ac4c28076182e242
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-07-26 00:47:47 +0700

    feat(qf): add mapping open all items and update colors qf

--- nvim/.config/nvim/after/ftplugin/qf.lua
+++ nvim/.config/nvim/after/ftplugin/qf.lua
@@ -57,3 +57,49 @@ end, {
   buffer = api.nvim_get_current_buf(),
   desc = "QF: open item",
 })
+
+local function expose_items_qf(t)
+  t = t or "split"
+
+  local items = vim.fn.getqflist()
+  local total_expose_item = 5
+
+  vim.cmd "wincmd p"
+  vim.cmd "cclose"
+  -- local total_items = #items
+  for i, item in pairs(items) do
+    local filename = vim.api.nvim_buf_get_name(item.bufnr)
+    if t == "tab" then
+      vim.cmd("tabnew " .. filename)
+      vim.api.nvim_win_set_cursor(0, { tonumber(item.lnum), tonumber(item.col) })
+    else
+      if tonumber(i) <= (total_expose_item - 1) then
+        vim.cmd(t .. " " .. filename)
+        vim.api.nvim_win_set_cursor(0, { tonumber(item.lnum), tonumber(item.col) })
+      end
+    end
+  end
+
+  if #items > total_expose_item and (t ~= "tab") then
+    local number_expose = total_expose_item - 1
+    RUtils.info(
+      "too many items, only expose " .. number_expose .. " from the total(" .. tostring(#items) .. ")",
+      { title = "QF" }
+    )
+    RUtils.map.feedkey("<c-w>=", "n")
+  end
+end
+
+RUtils.map.nnoremap("<Leader>wl", function()
+  expose_items_qf()
+end, {
+  buffer = api.nvim_get_current_buf(),
+  desc = "QF: open items qf with stack window",
+})
+
+RUtils.map.nnoremap("<Leader>wL", function()
+  expose_items_qf "tab"
+end, {
+  buffer = api.nvim_get_current_buf(),
+  desc = "QF: open items qf with tab window",
+})
--- nvim/.config/nvim/syntax/qf.vim
+++ nvim/.config/nvim/syntax/qf.vim
@@ -2,15 +2,17 @@ if exists('b:current_syntax')
     finish
 endif
 
-syn match qfFileName /^[^│]*/ nextgroup=qfSeparatorLeft
-syn match qfSeparatorLeft /│/ contained nextgroup=qfLineNr
-syn match qfLineNr /[^│]*/ contained nextgroup=qfSeparatorRight
-syn match qfSeparatorRight '│' contained nextgroup=qfError,qfWarning,qfInfo,qfNote
+syn match qfHeader /^─.*$/
+syn match qfFileName /^[^─][^┃]*/ nextgroup=qfSeparatorLeft
+syn match qfSeparatorLeft /┃/ contained nextgroup=qfLineNr
+syn match qfLineNr /[^┃]*/ contained nextgroup=qfSeparatorRight
+syn match qfSeparatorRight '┃' contained nextgroup=qfError,qfWarning,qfInfo,qfNote
 syn match qfError / E .*$/ contained
 syn match qfWarning / W .*$/ contained
 syn match qfInfo / I .*$/ contained
 syn match qfNote / [NH] .*$/ contained
 
+hi def link qfHeader Directory
 hi def link qfFileName Directory
 hi def link qfSeparatorLeft Delimiter
 hi def link qfSeparatorRight Delimiter
@@ -19,5 +21,7 @@ hi def link qfError DiagnosticError
 hi def link qfWarning DiagnosticWarn
 hi def link qfInfo DiagnosticInfo
 hi def link qfNote DiagnosticHint
+
+let b:current_syntax = 'qf'
 
 let b:current_syntax = 'qf'
