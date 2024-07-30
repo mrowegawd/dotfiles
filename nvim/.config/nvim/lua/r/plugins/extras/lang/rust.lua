f0b995ec71de9569bd2e80298297e35fc6008e7c
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-07-30 07:51:14 +0700

    delete unused

--- nvim/.config/nvim/lua/r/plugins/extras/lang/rust.lua
+++ nvim/.config/nvim/lua/r/plugins/extras/lang/rust.lua
@@ -139,25 +139,10 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- taplo = {
-        --   keys = {
-        --     {
-        --       "K",
-        --       function()
-        --         if vim.fn.expand "%:t" == "Cargo.toml" and require("crates").popup_available() then
-        --           require("crates").show_popup()
-        --         else
-        --           vim.lsp.buf.hover()
-        --         end
-        --       end,
-        --       desc = "LSP: show crate documentation [taplo]",
-        --     },
-        --   },
-        -- },
         taplo = {
           keys = {
             {
-              "K",
+              "gk",
               function()
                 if vim.fn.expand "%:t" == "Cargo.toml" and require("crates").popup_available() then
                   require("crates").show_popup()
