9c012d52a120ae5d7969ba403b4d2d6bf7eef08e
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-12-18 03:03:09 +0700

    feat(code): add `blink` and `cmp` integration

--- nvim/.config/nvim/lua/r/plugins/extras/ai/codeium.lua
+++ nvim/.config/nvim/lua/r/plugins/extras/ai/codeium.lua
@@ -1,16 +1,30 @@
 return {
   -- CODEIUM
   {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "Exafunction/codeium.nvim",
-        cmd = "Codeium",
-        build = ":Codeium Auth",
-        opts = {},
-      },
-    },
-    ---@param opts cmp.ConfigSchema
+    "Exafunction/codeium.nvim",
+    cmd = "Codeium",
+    build = ":Codeium Auth",
+    opts = {},
+  },
+
+  -- add ai_accept action
+  {
+    "Exafunction/codeium.nvim",
+    opts = function()
+      RUtils.cmp.actions.ai_accept = function()
+        if require("codeium.virtual_text").get_current_completion_item() then
+          RUtils.create_undo()
+          vim.api.nvim_input(require("codeium.virtual_text").accept())
+          return true
+        end
+      end
+    end,
+  },
+  -- codeium cmp source
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "codeium.nvim" },
     opts = function(_, opts)
       table.insert(opts.sources, 1, {
         name = "codeium",
@@ -19,4 +33,24 @@ return {
       })
     end,
   },
+  -- codeium blink source
+  vim.g.ai_cmp
+      and {
+        "saghen/blink.cmp",
+        optional = true,
+        dependencies = { "codeium.nvim", "saghen/blink.compat" },
+        opts = {
+          sources = {
+            compat = { "codeium" },
+            providers = {
+              codeium = {
+                kind = "Codeium",
+                score_offset = 100,
+                async = true,
+              },
+            },
+          },
+        },
+      }
+    or nil,
 }
