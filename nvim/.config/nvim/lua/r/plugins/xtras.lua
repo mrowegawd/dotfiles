4e9d19455310f0bafb9a1353d5451ae4ed143f93
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-12-25 16:24:12 +0700

    feat(xtras): follow lazy

--- nvim/.config/nvim/lua/r/plugins/xtras.lua
+++ nvim/.config/nvim/lua/r/plugins/xtras.lua
@@ -4,13 +4,19 @@ local Config = require "r.config"
 local prios = {
   ["r.plugins.extras.test.core"] = 1,
   ["r.plugins.extras.dap.core"] = 1,
+  ["r.plugins.extras.coding.nvim-cmp"] = 2,
   ["r.plugins.extras.ui.edgy"] = 2,
   ["r.plugins.extras.lang.typescript"] = 5,
+  ["r.plugins.extras.coding.blink"] = 5,
   ["r.plugins.extras.formatting.prettier"] = 10,
   -- default priority is 50
   ["r.plugins.extras.editor.aerial"] = 100,
   ["r.plugins.extras.editor.outline"] = 100,
 }
+
+if vim.g.xtras_prios then
+  prios = vim.tbl_deep_extend("force", prios, vim.g.xtras_prios or {})
+end
 
 ---@type string[]
 local extras = RUtils.dedup(Config.json.data.extras)
