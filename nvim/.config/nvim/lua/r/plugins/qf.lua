1b7585fdc664753cc435e64a290e85e6a2e83e37
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-01-09 22:18:53 +0700

    feat(qfsilet): add event

--- nvim/.config/nvim/lua/r/plugins/qf.lua
+++ nvim/.config/nvim/lua/r/plugins/qf.lua
@@ -10,6 +10,7 @@ return {
   -- QFSILET
   {
     dir = "~/.local/src/nvim_plugins/qfsilet",
+    event = "FileType qf",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
