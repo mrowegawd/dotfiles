f554537cc90944ae0b9778721a2b6fa1bc4be08a
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-01-15 22:25:23 +0700

    fix(sql): add condition when using `nvim-cmp`

--- nvim/.config/nvim/lua/r/plugins/extras/lang/sql.lua
+++ nvim/.config/nvim/lua/r/plugins/extras/lang/sql.lua
@@ -20,19 +20,21 @@ return {
       vim.api.nvim_create_autocmd("FileType", {
         pattern = sql_ft,
         callback = function()
-          local cmp = require "cmp"
+          if RUtils.has "nvim-cmp" then
+            local cmp = require "cmp"
 
-          -- global sources
-          ---@param source cmp.SourceConfig
-          local sources = vim.tbl_map(function(source)
-            return { name = source.name }
-          end, cmp.get_config().sources)
+            -- global sources
+            ---@param source cmp.SourceConfig
+            local sources = vim.tbl_map(function(source)
+              return { name = source.name }
+            end, cmp.get_config().sources)
 
-          -- add vim-dadbod-completion source
-          table.insert(sources, { name = "vim-dadbod-completion" })
+            -- add vim-dadbod-completion source
+            table.insert(sources, { name = "vim-dadbod-completion" })
 
-          -- update sources for the current buffer
-          cmp.setup.buffer { sources = sources }
+            -- update sources for the current buffer
+            cmp.setup.buffer { sources = sources }
+          end
         end,
       })
     end,
@@ -107,11 +109,7 @@ return {
       sources = {
         default = { "dadbod" },
         providers = {
-          dadbod = {
-            name = "Dadbod",
-            module = "vim_dadbod_completion.blink",
-            fallbacks = { "buffer" },
-          },
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
         },
       },
     },
