8163e0f632ac30d1ed628683d0f8d6820d57623c
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-01-18 22:15:55 +0700

    fix(sessions): use resession.nvim instead of auto-session

--- nvim/.config/nvim/lua/r/plugins/sessions.lua
+++ nvim/.config/nvim/lua/r/plugins/sessions.lua
@@ -2,9 +2,10 @@ return {
   --  ╭──────────────────────────────────────────────────────────╮
   --  │                         SESSION                          │
   --  ╰──────────────────────────────────────────────────────────╯
-  -- AUTO-SESSION
+  -- AUTO-SESSION (disabled)
   {
     "rmagatti/auto-session",
+    enabled = false,
     lazy = false,
     cmd = { "SessionSave", "SessionSearch", "SessionDelete", "SessionRestore", "SessionToggleAutoSave", "Autosession" },
     keys = {
@@ -75,6 +76,90 @@ return {
           },
         },
       }
+    end,
+  },
+  -- RESSESSION.NVIM
+  {
+    "stevearc/resession.nvim",
+    event = "VeryLazy",
+    opts = {
+      autosave = {
+        enabled = true,
+        notify = false,
+      },
+      buf_filter = function(bufnr)
+        if not require("resession").default_buf_filter(bufnr) then
+          return false
+        end
+        return true
+      end,
+      -- extensions = { quickfix = {} },
+      extensions = {},
+      options = { -- remove `cmdheight` from this
+        "binary",
+        "bufhidden",
+        "buflisted",
+        "diff",
+        "filetype",
+        "modifiable",
+        "previewwindow",
+        "readonly",
+        "scrollbind",
+        "winfixheight",
+        "winfixwidth",
+      },
+    },
+    config = function(_, opts)
+      local resession = require "resession"
+      resession.setup(opts)
+
+      vim.keymap.set("n", "<Leader>sS", function()
+        vim.ui.input({ prompt = "Session name" }, function(selected)
+          if selected then
+            resession.save(selected, {})
+          end
+        end)
+      end, { desc = "Misc: save with session name [resession.nvim]" })
+      --stylua: ignore
+      vim.keymap.set("n", "<Leader>ss", function() resession.save() end, { desc = "Misc: save session with current name [resession.nvim]" })
+      --stylua: ignore
+      vim.keymap.set("n", "<Leader>st", function() resession.save_tab() end, { desc = "Misc: save session tab [resession.nvim]" })
+      --stylua: ignore
+      vim.keymap.set("n", "<Leader>sl", function() resession.load() end, { desc = "Misc: load from list sessions [resession.nvim]" })
+      --stylua: ignore
+      -- vim.keymap.set("n", "<Leader>sl", function() resession.load "last" end, { desc = "Misc: load last session [resession.nvim]" })
+      vim.keymap.set("n", "<Leader>sd", resession.delete, { desc = "Misc: session delete [resession.nvim]" })
+
+      vim.api.nvim_create_user_command("SessionDetach", function()
+        resession.detach()
+      end, {})
+
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          -- Only load the session if nvim was started with no args
+          if vim.fn.argc(-1) == 0 then
+            -- Save these to a different directory, so our manual sessions don't get polluted
+            resession.load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
+          end
+        end,
+      })
+
+      if vim.tbl_contains(resession.list(), "__quicksave__") then
+        vim.defer_fn(function()
+          resession.load("__quicksave__", { attach = false })
+          local ok, err = pcall(resession.delete, "__quicksave__")
+          if not ok then
+            vim.notify(string.format("Error deleting quicksave session: %s", err), vim.log.levels.WARN)
+          end
+        end, 50)
+      end
+
+      RUtils.cmd.augroup("ResessionLeave", {
+        event = { "VimLeavePre" },
+        command = function()
+          resession.save "last"
+        end,
+      })
     end,
   },
 
