6d4eb0b9b30124fac57452f31a8a491a7573ade6
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-02-18 08:27:08 +0700

    fix(kitty): update mappings

--- kitty/.config/kitty/pass_keys.py
+++ kitty/.config/kitty/pass_keys.py
@@ -1,16 +1,20 @@
 import re
 
 from kittens.tui.handler import result_handler
-from kitty.fast_data_types import encode_key_for_tty
 from kitty.key_encoding import KeyEvent, parse_shortcut
 
+VIM_ID = "n?vim"
 
-def is_window_vim(window, vim_id):
+
+def is_window_vim(window):
     fp = window.child.foreground_processes
-    return any(re.search(vim_id, p['cmdline'][0], re.I) for p in fp)
+    return any(
+        re.search(VIM_ID, p["cmdline"][0] if len(p["cmdline"]) else "", re.I)
+        for p in fp
+    )
 
 
-def encode_key_mapping(key_mapping):
+def encode_key_mapping(window, key_mapping):
     mods, key = parse_shortcut(key_mapping)
     event = KeyEvent(
         mods=mods,
@@ -23,9 +27,7 @@ def encode_key_mapping(key_mapping):
         meta=bool(mods & 32),
     ).as_window_system_event()
 
-    return encode_key_for_tty(
-        event.key, event.shifted_key, event.alternate_key, event.mods, event.action
-    )
+    return window.encoded_key(event)
 
 
 def main():
@@ -37,12 +39,12 @@ def handle_result(args, result, target_window_id, boss):
     window = boss.window_id_map.get(target_window_id)
     direction = args[2]
     key_mapping = args[3]
-    vim_id = args[4] if len(args) > 4 else "n?vim"
 
     if window is None:
         return
-    if is_window_vim(window, vim_id):
-        encoded = encode_key_mapping(key_mapping)
-        window.write_to_child(encoded)
+    if is_window_vim(window):
+        for keymap in key_mapping.split(">"):
+            encoded = encode_key_mapping(window, keymap)
+            window.write_to_child(encoded)
     else:
         boss.active_tab.neighboring_window(direction)
