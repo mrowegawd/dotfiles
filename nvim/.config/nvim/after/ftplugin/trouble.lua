96b8aacb99e38054bf54ffa81b21cc36fecc8e17
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-04-03 21:21:35 +0700

    feat(trouble): add keybind `<c-q>`

--- nvim/.config/nvim/after/ftplugin/trouble.lua
+++ nvim/.config/nvim/after/ftplugin/trouble.lua
@@ -5,5 +5,37 @@ keymap.set("n", "<c-i>", "<Nop>", { buffer = api.nvim_get_current_buf() })
 keymap.set("n", "<c-o>", "<Nop>", { buffer = api.nvim_get_current_buf() })
 
 keymap.set("n", "<c-q>", function()
-  print(vim.inspet(require("trouble").get_items(opts)))
+  local tbl_items = {}
+  local prefix_title
+  if vim.bo.filetype == "trouble" then
+    local items = require("trouble").get_items()
+
+    for _, x in pairs(items) do
+      table.insert(tbl_items, {
+        bufnr = x.buf,
+        text = x.text,
+        lnum = x.pos[1],
+        col = x.pos[2],
+        filename = x.filename,
+      })
+
+      if prefix_title == nil then
+        prefix_title = x.source
+      end
+    end
+  end
+
+  if #tbl_items > 0 then
+    vim.fn.setqflist({}, "r", { title = "Trouble-" .. prefix_title, items = tbl_items })
+
+    local qf_win = RUtils.cmd.windows_is_opened { "trouble" }
+    if qf_win.found then
+      require("trouble").close()
+    end
+
+    local is_qf_opened = RUtils.cmd.windows_is_opened { "qf" }
+    if not is_qf_opened.found then
+      vim.cmd "copen"
+    end
+  end
 end, { buffer = api.nvim_get_current_buf(), desc = "Trouble: send to quickfix" })
