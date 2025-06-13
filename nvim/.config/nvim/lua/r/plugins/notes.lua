bcd3c1d3b154c65e763f80e751bcd1ac4327170a
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-06-14 05:42:18 +0700

    feat(orgmode): add custom border to agenda preview window

--- nvim/.config/nvim/lua/r/plugins/notes.lua
+++ nvim/.config/nvim/lua/r/plugins/notes.lua
@@ -35,6 +35,7 @@ return {
       return {
         ui = {
           input = { use_vim_ui = true }, -- menggunakan vim.ui.input nvim, jadi snacks.nvim yang handle nya
+          agenda = { preview_window = { border = RUtils.config.icons.border.line } },
           menu = {
             handler = function(data)
               local items = vim
