ab1b0271b52a68d29b14bebf6fc3e6a7d2bd6ced
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-06-20 21:18:22 +0700

    feat(smear-cursor): update configs

--- nvim/.config/nvim/lua/r/plugins/ui.lua
+++ nvim/.config/nvim/lua/r/plugins/ui.lua
@@ -222,7 +222,12 @@ return {
     "sphamba/smear-cursor.nvim",
     event = "LazyFile",
     cond = vim.g.neovide == nil and (os.getenv "TERMINAL" ~= "kitty"),
-    opts = true,
+    opts = {
+      stiffness = 0.8, -- 0.5      [0, 1]
+      trailing_stiffness = 0.5, -- 0.4      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
+      trailing_stiffness_insert_mode = 0.7, -- 0.55     [0, 1]
+    },
   },
   -- BLOCK.NVIM
   {
