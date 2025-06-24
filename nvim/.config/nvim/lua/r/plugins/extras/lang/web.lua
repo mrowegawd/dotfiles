27f3f3ead9d7d47a46eb93033a93c8a28472870c
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-06-24 23:27:44 +0700

    feat(django): add formatter for htmldjango

--- nvim/.config/nvim/lua/r/plugins/extras/lang/web.lua
+++ nvim/.config/nvim/lua/r/plugins/extras/lang/web.lua
@@ -2,7 +2,22 @@ return {
   -- TREESITTER
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "css", "html", "http", "scss", "nginx" } },
+    opts = { ensure_installed = { "css", "html", "http", "scss", "nginx", "htmldjango" } },
+  },
+
+  -- Formatter for html django
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "djlint" } },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        htmldjango = { "djlint" },
+      },
+    },
   },
 
   -- {
