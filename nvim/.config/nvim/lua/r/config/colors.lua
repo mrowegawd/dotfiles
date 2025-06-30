d0a937bcc3a4e388d4d66bdaa003ea1d8bb91cf7
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-07-01 05:03:33 +0700

    fix(colors): update color `@org.agenda.today`

--- nvim/.config/nvim/lua/r/config/colors.lua
+++ nvim/.config/nvim/lua/r/config/colors.lua
@@ -1242,8 +1242,8 @@ local general_overrides = function()
 
     {
       ["@org.agenda.today"] = {
-        fg = { from = "Boolean", attr = "fg", alter = 0.2 },
-        bg = { from = "Normal", attr = "bg", alter = 0.05 },
+        fg = { from = "@org.agenda.day", attr = "fg", alter = 0.6 },
+        bg = { from = "LineNr", attr = "fg", alter = 0.1 },
         bold = true,
         italic = true,
       },
