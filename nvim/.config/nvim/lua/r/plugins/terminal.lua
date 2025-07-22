94468f1529e2804078a8914a57910ef4668db029
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-07-23 02:46:07 +0700

    fix(terminal): update configs

--- nvim/.config/nvim/lua/r/plugins/terminal.lua
+++ nvim/.config/nvim/lua/r/plugins/terminal.lua
@@ -1,12 +1,46 @@
-local term_count = 1
+-- local term_count = 1
 
 return {
   -- ERGOTERM
   {
     "waiting-for-dev/ergoterm.nvim",
     opts = {
-      picker = "telescope",
+      picker = {
+        picker = "fzf-lua",
+      },
     },
+    config = function(_, opts)
+      require("ergoterm").setup(opts)
+
+      local terms = require "ergoterm.terminal"
+
+      local base = terms.Terminal:new {
+        name = "base",
+        cmd = "zsh",
+        layout = "below",
+        -- dir = "git_dir",
+      }
+
+      RUtils.map.vnoremap("<Localleader>t", function()
+        base:toggle()
+      end, { desc = "toggle base terminal" })
+      RUtils.map.nnoremap("<Localleader>t", function()
+        base:toggle()
+      end, { desc = "toggle base terminal" })
+      RUtils.map.tnoremap("<Localleader>t", function()
+        base:toggle()
+      end, { desc = "toggle base terminal" })
+
+      RUtils.map.vnoremap("<a-f>", function()
+        base:toggle()
+      end, { desc = "toggle base terminal" })
+      RUtils.map.nnoremap("<a-f>", function()
+        base:toggle()
+      end, { desc = "toggle base terminal" })
+      RUtils.map.tnoremap("<a-f>", function()
+        base:toggle()
+      end, { desc = "toggle base terminal" })
+    end,
   },
   -- FLOATERM (disabled)
   {
