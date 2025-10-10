2766d83318d8957b085dee3f22a5e54d71299540
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-10-11 01:57:55 +0700

    fix(yazi): trim path header

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -10,3 +10,27 @@ th.git.updated_sign = ""
 require("git"):setup {
   order = 500, -- order to show directory list. if 1500, gitsign go to rightmost
 }
+
+-- shorter header cwd
+function Header:cwd()
+  local max = self._area.w - self._right_width
+  if max <= 0 then
+    return ""
+  end
+
+  local s = tostring(ya.readable_path(tostring(self._current.cwd))):gsub("(%.?)([^/])[^/]+/", "%1%2/") .. self:flags()
+  return ui.Span(ui.truncate(s, { max = max, rtl = true })):style(th.mgr.cwd)
+end
+
+-- function Header:redraw()
+--   local right = self:children_redraw(self.RIGHT)
+--   self._right_width = right:width()
+--
+--   local left = self:children_redraw(self.LEFT)
+--
+--   return {
+--     ui.Line(left):area(self._area),
+--     ui.Line(right):area(self._area):align(ui.Align.RIGHT),
+--     table.unpack(ui.redraw(Progress:new(self._area, self._right_width))),
+--   }
+-- end
