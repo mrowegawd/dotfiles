cf0fee4c1169346fce3980a3271d57f94f879936
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-12-31 15:21:12 +0700

    feat(python): add django.nvim

--- nvim/.config/nvim/lua/r/plugins/extras/lang/python.lua
+++ nvim/.config/nvim/lua/r/plugins/extras/lang/python.lua
@@ -172,4 +172,55 @@ return {
       vim.g.quench_nvim_autostart_server = false
     end,
   },
+  -- DJANGO.NVIM
+  {
+    "mizisu/django.nvim",
+    -- event = "VeryLazy",
+    ft = "python",
+    opts = {
+      -- Default configuration
+      -- views = {
+      --   auto_refresh = {
+      --     on_picker_open = true,
+      --     file_watch_patterns = {
+      --       "*/urls.py",
+      --       "*/views.py",
+      --       "*/view.py",
+      --       "*/*views.py",
+      --       "*/*view.py",
+      --       "*/*viewset.py",
+      --       "*/*view_set.py",
+      --       "*/*api.py",
+      --     },
+      --   },
+      -- },
+      -- models = {
+      --   auto_refresh = {
+      --     on_picker_open = true,
+      --     file_watch_patterns = { "*/models.py", "*/models/*.py" },
+      --   },
+      -- },
+      -- shell = {
+      --   command = "shell",  -- "shell", "shell_plus", "shell_plus --ipython", etc.
+      --   position = "right", -- "bottom", "top", "left", "right", "float"
+      --   env = {},           -- { DJANGO_SETTINGS_MODULE = "myproject.settings" }
+      -- },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      sources = {
+        default = { "django" },
+        providers = {
+          django = {
+            name = "Django",
+            module = "django.completions.blink",
+            async = true,
+          },
+        },
+      },
+    },
+  },
 }
